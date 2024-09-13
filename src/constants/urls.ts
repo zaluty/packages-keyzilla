17196cf660f75e778b3794cdea80acb54133d38a
Hamza <hamzaredone6@gmail.com>
2024-09-13 08:28:28 +0000

    m

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3000"; 
-export const BASE_URLd = "https://keyzilla.vercel.app";
+export const BASE_URLd = "http://localhost:3000"; 
+export const BASE_URL = "https://keyzilla.vercel.app";
